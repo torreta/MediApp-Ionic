d61a3c3bf6411e43fd06a0ab399d1ceb1056ed73
Luis Campos <Luiscampostorreta@gmail.com>
2015-02-28 01:19:38 -0430

    added delete to medications

--- www/js/medicationsController.js
+++ www/js/medicationsController.js
@@ -48,204 +48,33 @@ angular.module('medications',[])
 
   }; //add medications
 
-  // $scope.setMedication = function(id){
+  $scope.deleteMedication = function(medication){
+    //console.log( "show en localst:" + localStorage.getItem("id") );
+      console.log("About to delete medication of name: " + medication.name );
+    if( confirm("you truly want to delete the medication?!") ){
+      $http({
+        method: 'DELETE',
+        url: BASE_URL + '/medications/1'
+      })
+
+      .success(function(data,status,headers,config){
+        console.log( "Medication Deleted successfully" );
+        console.log( data );
+        medication = [];
+      })
+
+      .error(function(data,status,headers,config){
+        console.log( "Error deleting medication" );
+        // If user doesnt have a token, create one and signin
+        //$scope.loginPOST();
+      });
+    };
+  }; //delete medications
 
-  //   localStorage.setItem("id",id);
-  //   console.log( "coloque con exito el valor " + localStorage.getItem("id")  );
-
-  // }; //add medications
-
-
-  // $scope.showMedication = function(){
-
-  //   //console.log( "show en localst:" + localStorage.getItem("id") );
-
-  //   if(localStorage.getItem("id") == undefined){
-
-  //     //console.log( "intente leer una agencia pero no habia valor" );
-  //     //localStorage.removeItem("id");
-
-  //   }else{
-  //       console.log("buscoAgencia de valor+ "+localStorage.getItem("id") );
-  //     $http({
-  //       method: 'GET',
-  //       url: BASE_URL + '/medications/'+localStorage.getItem("id")+".json",
-  //     })
-  //     .success(function(data,status,headers,config){
-  //       console.log( "agencia consultada con exito" );
-  //       console.log( data );
-  //       $scope.medication = data;
-
-  //       if(localStorageService.isSupported){
-  //           //console.log("Soporta Storage Service");
-  //           localStorage.setItem("id",$scope.medication.id);
-
-  //           // si te provoca borrar: localStorage.removeItem("id");
-  //           // localStorage.removeItem("id");
-  //         }else{
-  //           alert("Your browser does not support localStorage");
-  //         }
-  //       //deberiamos enviar el mensaje de CREADO!
-  //     })
-  //     .error(function(data,status,headers,config){
-  //       console.log( "error consulta agencia" );
-  //       // If user doesnt have a token, create one and signin
-  //       //$scope.loginPOST();
-  //     });
-  //   };
-
-  // }; //show medications
-
-
-  // $scope.updateMedication = function(medication){
-
-  //   //console.log( "show en localst:" + localStorage.getItem("id") );
-
-  //   if(localStorage.getItem("id") == undefined){
-
-  //     console.log( "intente leer una agencia pero no habia valor" );
-  //     //localStorage.removeItem("id");
-
-  //   }else{
-  //       console.log("busco actualizar Agencia de valor "+localStorage.getItem("id") );
-  //     $http({
-  //       method: 'PUT',
-  //       url: BASE_URL + '/medications/'+localStorage.getItem("id")+".json",
-  //       params:{
-  //       'name': $scope.medication.name,
-  //       'phone': $scope.medication.phone,
-  //       'address': $scope.medication.address
-  //       }
-  //     })
-  //     .success(function(data,status,headers,config){
-  //       console.log( "Medication updated successfully" );
-  //       console.log( data );
-  //       $scope.medication = data;
-
-  //       if(localStorageService.isSupported){
-  //           //console.log("Soporta Storage Service");
-  //           localStorage.setItem("id",$scope.medication.id);
-
-  //           //si te provoca borrar: localStorage.removeItem("id");
-  //            localStorage.removeItem("id");
-
-  //             $location.path('/medications');  
-  //         }else{
-  //           alert("Your browser does not support localStorage");
-  //         }
-  //       //deberiamos enviar el mensaje de CREADO!
-  //     })
-  //     .error(function(data,status,headers,config){
-  //       console.log( "error updating medication" );
-
-
-angular.module('medications',[])
-
-
-
-.controller('medicationController', function($scope,$http){
-
-
-
-    var BASE_URL = "http://torreta-163528.sae1.nitrousbox.com/api/v1";
-
-
-
-  //Consultos la agencias que se encuentran en el API
-
-  $scope.getMedications = function(){
-
-    console.log( "busco medicamentos" );
-
-    $scope.medications = [];
-
-
-
-    $http({
-
-      method: 'GET',
-
-      url: BASE_URL + '/medications',
-
-    })
-
-    .success(function(data,status,headers,config){
-
-      console.log( data );
-
-      $scope.medications = data;
-
-    })
-
-    .error(function(data,status,headers,config){
-
-      // If user doesnt have a token, create one and signin
-
-      //$scope.loginPOST();
-
-    });
-
-
-
-  }; //get medications
-
-
-
-  // $scope.addMedication = function(medication){
-
-  //   $http({
-
-  //     method: 'POST',
-
-  //     url: BASE_URL + '/medications',
-
-  //     params:{
-
-  //       'name': $scope.medication.name,
-
-  //       'phone': $scope.medication.phone,
-
-  //       'address': $scope.medication.address
-
-  //     }
-
-  //   })
-
-  //   .success(function(data,status,headers,config){
-
-  //     console.log( "Medication created" );
-
-  //     console.log( data );
-
-  //     mediapp.medications =  mediapp.medications + data;
-
-  //     $scope.medication = [];
-
-  //     $location.path('/medications'); 
-
-  //     //deberiamos enviar el mensaje de CREADO!
-
-  //   })
-
-  //   .error(function(data,status,headers,config){
-
-  //     console.log( "error creating Medication" );
-
-  //     // If user doesnt have a token, create one and signin
-
-  //     //$scope.loginPOST();
-
-  //   });
-
-
-
-  // }; //add medications
 
 
 
   // $scope.setMedication = function(id){
-
-
 
   //   localStorage.setItem("id",id);
 
@@ -435,79 +264,6 @@ angular.module('medications',[])
 
 
 
-  // $scope.deleteMedication = function(id){
-
-
-
-  //   //console.log( "show en localst:" + localStorage.getItem("id") );
-
-
-
-  //     console.log("busco borrar medication de valor "+id );
-
-
-
-  //   if( confirm("you truly want to delete the medication?!") ){
-
-  //     $http({
-
-  //       method: 'DELETE',
-
-  //       url: BASE_URL + '/medications/'+id,
-
-  //     })
-
-  //     .success(function(data,status,headers,config){
-
-  //       console.log( "agencia Borrada con exito" );
-
-  //       console.log( data );
-
-  //       $scope.medication = data;
-
-
-
-  //       if(localStorageService.isSupported){
-
-  //           //console.log("Soporta Storage Service");
-
-  //           localStorage.setItem("id",$scope.medication.id);
-
-
-
-  //           //si te provoca borrar: localStorage.removeItem("id");
-
-  //            localStorage.removeItem("id");
-
-
-
-  //            $scope.getMedications(); 
-
-  //         }else{
-
-  //           alert("Your browser does not support localStorage");
-
-  //         }
-
-  //       //deberiamos enviar el mensaje de CREADO!
-
-  //     })
-
-  //     .error(function(data,status,headers,config){
-
-  //       console.log( "error deleting medication" );
-
-  //       // If user doesnt have a token, create one and signin
-
-  //       //$scope.loginPOST();
-
-  //     });
-
-  //   };
-
-
-
-  // }; //delete medications
 
 
 
@@ -521,57 +277,4 @@ angular.module('medications',[])
 
   });//controller
 
-
-
-
-  //       // If user doesnt have a token, create one and signin
-  //       //$scope.loginPOST();
-  //     });
-  //   };
-
-  // }; //update medications
-
-  // $scope.deleteMedication = function(id){
-
-  //   //console.log( "show en localst:" + localStorage.getItem("id") );
-
-  //     console.log("busco borrar medication de valor "+id );
-
-  //   if( confirm("you truly want to delete the medication?!") ){
-  //     $http({
-  //       method: 'DELETE',
-  //       url: BASE_URL + '/medications/'+id,
-  //     })
-  //     .success(function(data,status,headers,config){
-  //       console.log( "agencia Borrada con exito" );
-  //       console.log( data );
-  //       $scope.medication = data;
-
-  //       if(localStorageService.isSupported){
-  //           //console.log("Soporta Storage Service");
-  //           localStorage.setItem("id",$scope.medication.id);
-
-  //           //si te provoca borrar: localStorage.removeItem("id");
-  //            localStorage.removeItem("id");
-
-  //            $scope.getMedications(); 
-  //         }else{
-  //           alert("Your browser does not support localStorage");
-  //         }
-  //       //deberiamos enviar el mensaje de CREADO!
-  //     })
-  //     .error(function(data,status,headers,config){
-  //       console.log( "error deleting medication" );
-  //       // If user doesnt have a token, create one and signin
-  //       //$scope.loginPOST();
-  //     });
-  //   };
-
-  // }; //delete medications
-
-  
-
-
-
-  });//controller
 
